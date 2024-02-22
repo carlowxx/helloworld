ce0495aa57bf47d6bc46efb95f5df80b72ebf6b6
carlowxx <cardimi.maria2016@outlook.com>
2024-02-22 18:18:57 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<?php 
+require("_global.php");
+//configurações dessa página
+$page = array(
+    "title" => "Lendo e Aprendendo",    //titulo dessa página
+    "css" => "index.css" ,              //folha d estilos 
+    "js" => "index.css",                 //javascript desta página
+    
+
+);
+
+
+//inclui o cabeçalho do documento
+
+require("_header.php");?>
+
+<article>
+   
+
+</article>
+
+<aside>
+    
+</aside>
+<?php require ("_footer.php");
+?>
